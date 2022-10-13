291bc3ae402378a2f0999b4712c08b3cdcf950fc
pragatibhandari <bhandariprag7@gmail.com>
2022-10-14 00:21:06 +0300

    error handeling

--- server.js
+++ server.js
@@ -7,4 +7,15 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("text").send("404 not found");
+  }
+});
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
