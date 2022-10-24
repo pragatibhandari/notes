20578086687d372cdd30fbfd2038a8ee7fb824d2
pragatibhandari <bhandariprag7@gmail.com>
2022-10-24 23:45:20 +0300

    changes in logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,3 +3,8 @@ const { v4: uuid } = require("uuid");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
+
+const logEvents = async (message, logFileName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+};
